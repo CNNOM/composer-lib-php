f7f8c6ac4e1506d0fcc6050d930b94456fe8664d
Алексей Зенков <zenkov.m700@mail.ru>
2025-12-05 18:02:53 +0300

    Fix length calculation bug

--- src/KingPenis.php
+++ src/KingPenis.php
@@ -4,8 +4,13 @@ namespace Cnnom\LibPenis;
 
 class KingPenis
 {
-    public function getLength(string $s): int  
-    {  
-        return strlen($s);  
-    }  
+    public function getLength(string $s): int
+    {
+        return strlen($s);
+    }
+
+    public function getLengthFix(string $s): int
+    {
+        return mb_strlen($s);
+    }
 }
